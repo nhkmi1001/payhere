e1b6a5ddaf6f5a5563313bf1448a076127f817fd
nhkmi1001 <nhkmi1001@gmail.com>
2023-01-19 12:03:05 +0900

    [MOD] settings.py

--- users/models.py
+++ users/models.py
@@ -1,3 +1,51 @@
 from django.db import models
+from django.contrib.auth.models import BaseUserManager, AbstractBaseUser
 
-# Create your models here.
+class UserManager(BaseUserManager):
+    def create_user(self, email, password=None):
+        
+        user = self.model(email=email)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password=None):
+        user = self.create_user(
+            email=email,
+            password=password,
+        )
+        user.is_admin = True
+        user.save(using=self._db)
+        return user
+    
+class TimestampZone(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
+    class Meta:
+        abstract = True
+        
+class User(TimestampZone):
+    email = models.EmailField("이메일", max_length=256, unique=True)
+    is_admin = models.BooleanField("관리자", default=False)
+    is_active = models.BooleanField("관리자", default=True)
+    
+    objects = UserManager()
+    
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = ["email"]
+    
+    def __str__(self):
+        return self.email
+
+    def has_perm(self, perm, obj=None):
+        return True
+
+    def has_module_perms(self, app_label):
+        return True
+
+    @property
+    def is_staff(self):
+
+        return self.is_admin
+    
